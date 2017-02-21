08cafd6be48a62cc2303512a66d740d7dc0e39eb
Jens Dutzi <jens.dutzi@tf-network.de>
2017-02-21 15:28:02 +0100

    Some changes for Contao 3.5.24 and PHP 7.1 support, hide unnecessary information from user-model inside the activation email (some new fields)  and some cosmetic corrections (whitespaces, copyright-year etc.).

--- TL_ROOT/system/modules/nc_registration_notify_admin/languages/de/default.php
+++ TL_ROOT/system/modules/nc_registration_notify_admin/languages/de/default.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 
 /**
  * Contao Open Source CMS
- * 
+ *
  * Copyright (C) 2005-2012 Leo Feyer
- * 
- * @package   NC Registration Admin Notification 
+ *
+ * @package   NC Registration Admin Notification
  * @author    Marcel Mathias Nolte
- * @copyright Marcel Mathias Nolte 2015
+ * @copyright Marcel Mathias Nolte 2017
  * @website	  https://www.noltecomputer.com
  * @license   <marcel.nolte@noltecomputer.de> wrote this file. As long as you retain this notice you
- *            can do whatever you want with this stuff. If we meet some day, and you think this stuff 
+ *            can do whatever you want with this stuff. If we meet some day, and you think this stuff
  *            is worth it, you can buy me a beer in return. Meanwhile you can provide a link to my
  *            homepage, if you want, or send me a postcard. Be creative! Marcel Mathias Nolte
  */
